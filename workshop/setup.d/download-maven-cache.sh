0744f44aaf73a1c74cbb989f609beb711ec8ca86
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-07-22 16:07:57 +1000

    Set Maven cache image location from workshop definition.

--- workshop/setup.d/download-maven-cache.sh
+++ workshop/setup.d/download-maven-cache.sh
@@ -2,4 +2,9 @@
 
 set -x
 
-time imgpkg pull -i ghcr.io/eduk8s-labs/eduk8s-labs/lab-graalvm-testing-m2:latest -o $HOME/.m2
+# The MAVEN_CACHE_IMAGE environment variable is set from the Workshop
+# definition so the location can be overridden. This allows the image to
+# be copied into an image registry in the same cluster as Educates is
+# running, speeding up the download.
+
+time imgpkg pull -i $MAVEN_CACHE_IMAGE -o $HOME/.m2
