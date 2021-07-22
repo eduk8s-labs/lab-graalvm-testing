f893da49b9c7c0e44ffe838798ed325da900eb48
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-07-22 14:36:14 +1000

    Switch to Petclinic app.

--- workshop/setup.d/download-maven-cache.sh
+++ workshop/setup.d/download-maven-cache.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-time imgpkg pull -i ghcr.io/eduk8s-labs/eduk8s-labs/lab-graalvm-testing-m2:latest
--o $HOME/.m2
+time imgpkg pull -i ghcr.io/eduk8s-labs/eduk8s-labs/lab-graalvm-testing-m2:latest -o $HOME/.m2
